d63222d8459d12c2a99b6f28103749c0867af182
Jorge Lucas <jorge.pereira@sumicity.net.br>
2019-06-23 21:37:49 -0300

    Create swagger.

--- src/main/java/com/gado/apirest/controllers/SaidaController.java
+++ src/main/java/com/gado/apirest/controllers/SaidaController.java
@@ -2,6 +2,8 @@ package com.gado.apirest.controllers;
 
 import com.gado.apirest.models.Saida;
 import com.gado.apirest.repositories.SaidaRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,32 +11,39 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/gado")
+@Api(value = "API REST Saídas")
+@CrossOrigin(origins = "*")
 public class SaidaController {
 
     @Autowired
     SaidaRepository saidaRepository;
 
     @GetMapping("/saidas")
+    @ApiOperation(value = "Retorna uma lista de 'Saídas'")
     public List<Saida> listaSaida() {
         return saidaRepository.findAll();
     }
 
     @GetMapping("/saida/{id}")
+    @ApiOperation(value = "Retorna uma única 'Saída', passando um id como parâmetro")
     public Saida listaSaidaUnica(@PathVariable(value = "id") long id) {
         return saidaRepository.findById(id);
     }
 
     @PostMapping("/saida")
+    @ApiOperation(value = "Salva uma 'Saída'")
     public Saida salvaSaida(@RequestBody Saida saida) {
         return saidaRepository.save(saida);
     }
 
     @DeleteMapping("/saida")
+    @ApiOperation(value = "Deleta uma 'Saída'")
     public void deletaSaida(@RequestBody Saida saida) {
         saidaRepository.delete(saida);
     }
 
     @PutMapping("/saida")
+    @ApiOperation(value = "Atualiza uma 'Saída'")
     public Saida atualizaSaida(@RequestBody Saida saida) {
         return saidaRepository.save(saida);
     }
--- src/main/java/com/gado/apirest/controllers/VendedorController.java
+++ src/main/java/com/gado/apirest/controllers/VendedorController.java
@@ -2,6 +2,8 @@ package com.gado.apirest.controllers;
 
 import com.gado.apirest.models.Vendedor;
 import com.gado.apirest.repositories.VendedorRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,32 +11,39 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/gado")
+@Api(value = "API REST Gados")
+@CrossOrigin(origins = "*")
 public class VendedorController {
 
     @Autowired
     VendedorRepository vendedorRepository;
 
     @GetMapping("/vendedores")
+    @ApiOperation(value = "Retorna uma lista de 'Vendedores'")
     public List<Vendedor> listaVendedor() {
         return vendedorRepository.findAll();
     }
 
     @GetMapping("/vendedor/{id}")
+    @ApiOperation(value = "Retorna um único 'Vendedor', passando um id como parâmetro")
     public Vendedor listaVendedorUnico(@PathVariable(value = "id") long id) {
         return vendedorRepository.findById(id);
     }
 
     @PostMapping("/vendedor")
+    @ApiOperation(value = "Salva um 'Vendedor'")
     public Vendedor salvaVendedor(@RequestBody Vendedor vendedor) {
         return vendedorRepository.save(vendedor);
     }
 
     @DeleteMapping("/vendedor")
+    @ApiOperation(value = "Deleta um 'Vendedor'")
     public void deletaVendedor(@RequestBody Vendedor vendedor) {
         vendedorRepository.delete(vendedor);
     }
 
     @PutMapping("/vendedor")
+    @ApiOperation(value = "Atualiza um 'Vendedor'")
     public Vendedor atualizaVendedor(@RequestBody Vendedor vendedor) {
         return vendedorRepository.save(vendedor);
     }
